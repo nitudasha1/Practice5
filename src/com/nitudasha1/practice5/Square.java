7e33a78a64bed6a2dee5972319fd324a7e14ad4e
Dasha Ershova <dara-ershova@mail.ru>
2020-10-03 01:47:47 +0300

    Initial commit

--- src/com/nitudasha1/practice5/Square.java
+++ src/com/nitudasha1/practice5/Square.java
@@ -1,31 +1,41 @@
 package com.nitudasha1.practice5;
 
-public class Square extends Rectangle {
+public class Square extends Shape {
+    protected double side;
 
     public Square() {
     }
 
     public Square(double side) {
-        super(side, side);
+       this.side=side;
     }
 
     public Square(String color, boolean filled, double side) {
-        super(color, filled, side, side);
+        super(color, filled);
+        this.side=side;
     }
 
     public double getSide() {
-        return length;
+        return side;
     }
 
     public void setSide(double side) {
-        this.length = side;
-        this.width = side;
+        this.side = side;
+    }
+    @Override
+    public double getArea() {
+        return Math.pow(side, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * side;
     }
 
     @Override
     public String toString() {
         return "Square{" +
-                "side=" + width +
+                "side=" + side +
                 ", color='" + color + '\'' +
                 ", filled=" + filled +
                 '}';
